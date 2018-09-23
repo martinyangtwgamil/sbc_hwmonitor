f562a9b4b2a510c850cddbd0894abe0f689d934f
Martin.Yang <martin.yang@axiomtek.com.tw>
2018-09-23 08:59:20 -0700

    remove old file

--- home/controller/src/iptable_nat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#tested interface named enp0s25
-iptables -F
-iptables -t nat -A POSTROUTING -s 192.168.191.0/24 -o enp0s25 -j SNAT --to 10.237.0.1
-iptables -t nat -A PREROUTING -d 10.237.0.1 -i enp0s25 -p tcp --dport 8888 -j DNAT --to 192.168.191.10
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P INPUT ACCEPT
